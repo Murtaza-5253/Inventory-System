22e29c96e39a69bee2211a4ffa8b1978bea8afb9
Murtaza <murtazamaimoon73@gmail.com>
2025-07-17 21:00:11 +0530

    Added Supplier Entity

---
+++ src/main/java/com/mz/inventory/controller/SupplierController.java
@@ -0,0 +1,25 @@
+package com.mz.inventory.controller;
+
+import com.mz.inventory.model.Supplier;
+import com.mz.inventory.service.SupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/suppliers")
+public class SupplierController {
+    @Autowired
+    private SupplierService supplierService;
+
+    @GetMapping
+    public List<Supplier> getAllSuppliers() {
+        return supplierService.getAllSuppliers();
+    }
+
+    @PostMapping
+    public Supplier addSupplier(@RequestBody Supplier supplier) {
+        return supplierService.addSupplier(supplier);
+    }
+}
---
+++ src/main/java/com/mz/inventory/model/Supplier.java
@@ -0,0 +1,34 @@
+package com.mz.inventory.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "suppliers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name should not be blank")
+    @Size(min = 2, max = 100)
+    private String name;
+    @NotBlank(message = "Email should not be blank")
+    @Email
+    private String email;
+    @NotBlank(message = "Phone number should not be blank")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be exactly 10 digits")
+    private String phone;
+    @NotBlank(message = "Address should not be blank")
+    @Size(max = 200)
+    private String address;
+}
